c225219624ae17a255c77035b9ce21728a900709
Jiayich0 <jiayich.p@gmail.com>
2025-03-11 16:33:49 +0100

    Update: pr2 - init

--- Pr2/src/simulator/control/Controller.java
+++ Pr2/src/simulator/control/Controller.java
@@ -70,7 +70,6 @@ public class Controller {
 	    for (int i = 0; i < n - 1; i++) {
 	        _sim.advance();
 	        p.print(_sim.report());
-	        System.out.println("Linea JSON: " + i + " -> " + _sim.report().toString()); // Para depurar: breakpoint porperties, conditional, i == 137
 	        p.println(",");
 	    }
 	    
