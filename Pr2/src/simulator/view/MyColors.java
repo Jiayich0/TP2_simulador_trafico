d175582578f59c9abf39efae3a0f32a9da3d9149
Jiayich0 <jiayich.p@gmail.com>
2025-04-09 00:32:10 +0200

    Create: MyColors
    Paleta de colores de Discord

---
+++ Pr2/src/simulator/view/MyColors.java
@@ -0,0 +1,14 @@
+package simulator.view;
+
+import java.awt.Color;
+
+public class MyColors  {
+
+	public static final Color AZUL = new Color(0x72, 0x89, 0xDA);    	// #7289da
+    public static final Color BLANCO = Color.WHITE;                 	// #ffffff
+    public static final Color GRIS_CLARO = new Color(0x99, 0xaa, 0xb5); // #99aab5
+    public static final Color GRIS = new Color(0x2c, 0x2f, 0x33);       // #2c2f33
+    public static final Color NEGRO = new Color(0x23, 0x27, 0x2a);     	// #23272a
+
+    private MyColors() {}
+}
